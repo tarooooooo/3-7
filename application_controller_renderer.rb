cd3b2106510477ab0a5388aa41595df987f0780d
RyutaroMaeda <ryutaromaeda83@gmail.com>
2021-06-12 11:25:56 +0900

    [finish]3-7

---
+++ application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'debb43994b73d764d45652702ef2964f82e0110e245f4b4d3d7abda70f9b124da70abb6ff7ea846ce90e8fc0924c2dc560b0a811e1f9ee36dd7c3d1d3243e41c'
